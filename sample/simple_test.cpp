3bcf6772c19a9d0390ab1fd2d178635ece537fa4
ko jung hyun <jeremyko69@gmail.com>
2020-05-22 15:50:02 +0900

    sample changed

--- sample/simple_test.cpp
+++ sample/simple_test.cpp
@@ -25,16 +25,7 @@
 #include <atomic>
 #include "KoThreadPool.hpp"
 
-std::atomic<int> gSum1 ;
-std::atomic<int> gSum2 ;
-std::atomic<int> gSum3 ;
-
-///////////////////////////////////////////////////////////////////////////////
-void SumWork1()
-{
-    gSum1++;
-    //std::this_thread::sleep_for(std::chrono::milliseconds(1));
-}
+std::atomic<int> g_sum ;
 
 ///////////////////////////////////////////////////////////////////////////////
 class MyClass
@@ -42,20 +33,15 @@ class MyClass
     public:
         MyClass()  {} ;
         ~MyClass() {} ;
-        void SumWork2() {
-            gSum2++;
-        }
-        void SumWork3(int a) {
-            gSum3 += a;
+        void SumWork( int a) {
+            g_sum += a;
         }
 };
 
 ///////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    gSum1 = 0;
-    gSum2 = 0;
-    gSum3 = 0;
+    g_sum = 0;
     MyClass      myclass ;
     KoThreadPool tpool   ;
 
@@ -64,46 +50,19 @@ int main()
         exit(1);
     }
 
-    for(int i = 0; i < 100; i++ ){
-        tpool.SetWaitingCnt(10); //set total work count    
-        //to make gSum1 = 4
-        std::function<void()> temp_func1 = std::bind( &SumWork1 ) ;
-        tpool.AssignTask(temp_func1 )  ;
-        std::function<void()> temp_func2 = std::bind( &SumWork1 ) ;
-        tpool.AssignTask(temp_func2 )  ;
-        std::function<void()> temp_func3 = std::bind( &SumWork1 ) ;
-        tpool.AssignTask(temp_func3 )  ;
-        std::function<void()> temp_func4 = std::bind( &SumWork1 ) ;
-        tpool.AssignTask(temp_func4); 
-        //to make gSum2 = 3
-        std::function<void()> temp_func5 = std::bind( &MyClass::SumWork2, &myclass) ;
-        tpool.AssignTask(temp_func5 )  ;
-        std::function<void()> temp_func6 = std::bind( &MyClass::SumWork2, &myclass) ;
-        tpool.AssignTask(temp_func6 )  ;
-        std::function<void()> temp_func7 = std::bind( &MyClass::SumWork2, &myclass) ;
-        tpool.AssignTask(temp_func7 )  ;
-        //to make gSum3 = 3
-        std::function<void()> temp_func8 = std::bind( &MyClass::SumWork3, &myclass,1) ;
-        tpool.AssignTask(temp_func8 )  ;
-        std::function<void()> temp_func9 = std::bind( &MyClass::SumWork3, &myclass,1) ;
-        tpool.AssignTask(temp_func9 )  ;
-        std::function<void()> temp_func10 = std::bind( &MyClass::SumWork3, &myclass,1) ;
-        tpool.AssignTask(temp_func10 )  ;
+    tpool.SetWaitingCnt(10); //set total work count    
 
-        //wait all works done.
-        tpool.WaitAllWorkDone(); // --> blocking call. 
-        if(gSum1 !=4 || gSum2 !=3 || gSum3 !=3){ //to vefify
-            std::cout << i << " : error  : " << gSum1 << "," << gSum2 << "," <<gSum3 << "\n";
-            exit(1);
-        }
-        // -------------------------------    
-        // XXX your other work here.... 
-        // -------------------------------    
-
-        gSum1 = 0;
-        gSum2 = 0;
-        gSum3 = 0;
+    for(int i = 0; i < 10; i++ ){
+        std::function<void()> temp_func = std::bind( &MyClass::SumWork, &myclass,1) ;
+        tpool.AssignTask(temp_func)  ;
     }
+    //wait all works done.
+    tpool.WaitAllWorkDone(); // --> blocking call. 
+    if(g_sum != 10){ //to vefify
+        std::cout << "error  : " << g_sum << "\n";
+        exit(1);
+    }
+
     std::cout << "all work done \n" ;
     //time to program exit, terminate thread pool.
     tpool.Terminate(); //graceful terminate : wait until all work done
